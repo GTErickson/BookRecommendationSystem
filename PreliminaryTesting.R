d723534f101af034144815cdee71f5568f8c1b83
SilasPetersonWSU <122935334+SilasPetersonWSU@users.noreply.github.com>
2023-11-29 18:51:54 -0800

    Added a user creation to PreliminaryTesting.R

--- PreliminaryTesting.R
+++ PreliminaryTesting.R
@@ -1,41 +1,63 @@
-#Set to proper working directory
 getwd()
 setwd("C:/Users/gmone/475/Project")
 
-#Load proper packages
+# Load packages
 library(tidyverse)
 
-#Load data sets
+# Load datasets
 books <- read.csv("BX_Books.csv", sep = ";", header = TRUE)
 ratings <- read.csv("BX-Book-Ratings.csv", sep = ";", header = TRUE)
 users <- read.csv("BX-Users.csv", sep = ";", header = TRUE)
 
-#Get general rating data frame
-x = ratings %>%
-  group_by(ISBN) %>%  #Group ratings based on book
-  summarise(avg_rating = mean(Book.Rating)) %>%  #Get average rating for each book
-  filter(ISBN %in% books$ISBN)  #Clean data for poor rating entries
+# Prompt user to create a profile
+cat("Welcome! Let's create your profile.\n")
 
-y = ratings %>%
+# Get user location
+cat("Enter your location details:\n")
+location <- readline(prompt = "Location (City, State, Country): ")
+
+# Get user age
+user_age <- as.numeric(readline(prompt = "Enter your age: "))
+
+# Create a new user profile with fixed ID 0
+new_user <- data.frame(User.ID = 0, Location = location, Age = user_age)
+
+# Insert the new user at the top of the users dataframe
+users <- rbind(new_user, users)
+
+# Display the updated users dataframe
+cat("\nUser Profile Created:\n")
+print(new_user)
+print(users)
+
+# Data cleaning and processing for general ratings
+book_ratings <- ratings %>%
+  group_by(ISBN) %>%
+  summarise(avg_rating = mean(Book.Rating)) %>%
+  filter(ISBN %in% books$ISBN)
+
+book_counts <- ratings %>%
   group_by(ISBN) %>%
   count()
 
-x$count = y$n
+book_ratings$count <- book_counts$n
 
-head(x) 
+# Display the first few rows of the general rating data frame
+head(book_ratings)
 
-#Now general rating algorithm
-out = x %>%
+# General rating algorithm
+general_ratings <- book_ratings %>%
   filter(avg_rating > 8) %>%
   filter(count > 1)
-  
 
-#Now we want it to get user specific (User # )
-
-y = ratings %>%
+# User-specific ratings (incomplete, you may need to define what you want to do with the count)
+user_counts <- ratings %>%
   group_by(User.ID) %>%
   count()
 
-x = ratings %>%
+user_ratings <- ratings %>%
   group_by(User.ID) %>%
   summarise(avg_rating = mean(Book.Rating))
+
+# Display the first few rows of user-specific ratings
+head(user_ratings)
